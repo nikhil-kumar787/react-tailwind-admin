7e651f6bf3e835fb16022f9b6272ee1b9fe9882a
Nikhil <nikhilkumar7167@gmail.com>
2021-05-29 18:17:09 +0530

    Added react-tailwind-admin app

---
+++ src/views/AdminDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Dashboard from './admin/Dashboard'
+
+function AdminDashboard() {
+    return (
+        <div>
+            <Dashboard></Dashboard>
+        </div>
+    )
+}
+
+export default AdminDashboard
